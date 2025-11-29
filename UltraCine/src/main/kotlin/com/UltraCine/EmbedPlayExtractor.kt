66b919a2ea118564b853ffdfc487a24cc06b9d5c
euluan1912 <euluan1912@gmail.com>
2025-11-29 14:23:17 -0300

    Atualizar o EmbedPlayExtractor.kt

--- UltraCine/src/main/kotlin/com/UltraCine/EmbedPlayExtractor.kt
+++ UltraCine/src/main/kotlin/com/UltraCine/EmbedPlayExtractor.kt
@@ -2,11 +2,13 @@ package com.UltraCine
 
 import com.lagradost.cloudstream3.extractors.VidStack
 
+// Esta classe lida com o domínio upns.pro
 class EmbedPlayUpnsPro : VidStack() {
     override var name = "EmbedPlay UpnsPro"
     override var mainUrl = "https://embedplay.upns.pro"
 }
 
+// Esta classe lida com o domínio upn.one
 class EmbedPlayUpnOne : VidStack() {
     override var name = "EmbedPlay UpnOne"
     override var mainUrl = "https://embedplay.upn.one"
