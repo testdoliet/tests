4015779708e0c6ec545807eaa8a1135b37c5fbe9
L <euluan1912@gmail.com>
2025-12-06 09:30:55 -0300

    Atualizar o UltraCineProvider.kt

--- UltraCine/src/main/kotlin/com/UltraCine/UltraCineProvider.kt
+++ UltraCine/src/main/kotlin/com/UltraCine/UltraCineProvider.kt
@@ -1,17 +1,25 @@
-
 package com.UltraCine
 
 import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+// ADICIONE AS IMPORTAÇÕES NECESSÁRIAS AQUI:
+import com.lagradost.cloudstream3.extractors.ExtractorApi
+import com.lagradost.cloudstream3.MainAPI
+
+// Importa as classes de extratores que você criou no mesmo pacote
+import com.UltraCine.PlayEmbedApiSite // Certifique-se que o nome do arquivo/classe é este
+import com.UltraCine.EmbedPlayExtractor // Certifique-se que o nome do arquivo/classe é este
 
 @CloudstreamPlugin
 class UltraCineProvider : BasePlugin() {
-        override fun load() {
-        registerMainAPI(UltraCine())
+    override fun load() {
+        // CORRIGE UNRESOLVED REFERENCE PARA UltraCine
+        registerMainAPI(UltraCine()) 
         
-        // 1. REGISTRA A CLASSE QUE LIDA COM 'playembedapi.site'
-        registerExtractorAPI(PlayEmbedApiSite()) 
+        // CORRIGE UNRESOLVED REFERENCE PARA PlayEmbedApiSite
+        registerExtractorAPI(PlayEmbedApiSite())
         
-        // 2. REGISTRA A CLASSE QUE LIDA COM 'embedplay.upns.ink'
-        registerExtractorAPI(EmbedPlayExtractor()) // Use o nome exato da classe no seu EmbedPlayExtractor.kt
-    }
+        // CORRIGE UNRESOLVED REFERENCE PARA EmbedPlayExtractor
+        registerExtractorAPI(EmbedPlayExtractor()) 
+    } // <--- CHAVE DE FECHAMENTO CORRETA PARA O override fun load()
+} // <--- CHAVE DE FECHAMENTO CORRETA PARA A CLASSE UltraCineProvider
