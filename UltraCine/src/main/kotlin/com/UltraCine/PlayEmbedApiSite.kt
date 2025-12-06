5f866207e96eb6437d9425156d097df6d8823482
L <euluan1912@gmail.com>
2025-12-06 10:50:34 -0300

    Atualizar o PlayEmbedApiSite.kt

--- UltraCine/src/main/kotlin/com/UltraCine/PlayEmbedApiSite.kt
+++
@@ -1,8 +0,0 @@
-package com.UltraCine.extractors
-
-import com.lagradost.cloudstream3.extractors.VidStack
-
-class PlayEmbedApiSite : VidStack() {
-    override var name = "Play Embed API"
-    override var mainUrl = "http://playembedapi.site" // Use http ou https conforme a URL do site
-}
