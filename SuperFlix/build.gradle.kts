ab79673f4b27a4f48efcf59bd2de764295c2b074
testdoliet <contagamer5903@gmail.com>
2025-12-13 14:04:09 -0300

    Update build.gradle.kts

--- SuperFlix/build.gradle.kts
+++ SuperFlix/build.gradle.kts
@@ -44,11 +44,3 @@ cloudstream {
     isCrossPlatform = true
     requiresResources = true
 }
-
-dependencies {
-    // FIXME remove this when crossplatform is fully supported
-    val cloudstream by configurations
-    implementation("com.google.android.material:material:1.13.0")
-    implementation("androidx.browser:browser:1.9.0")
-    cloudstream("com.lagradost:cloudstream3:pre-release")
-}
