614d409d43b17cae7b9a155a53baee2fcb005743
testdoliet <contagamer5903@gmail.com>
2025-12-14 14:52:51 -0300

    Update and rename AnimeFirePlugin.kt to AnimeFireProvider.kt

--- AnimeFire/src/main/kotlin/com/AnimeFire/AnimeFireProvider.kt
+++ AnimeFire/src/main/kotlin/com/AnimeFire/AnimeFireProvider.kt
@@ -5,7 +5,7 @@ import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class AnimeFirePlugin: Plugin() {
+class AnimeFireProvider: Plugin() {
     override fun load(context: Context) {
         registerMainAPI(AnimeFire())
     }
