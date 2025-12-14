f38e5308a6a6e9562fc1cc316031839eafe073d5
testdoliet <contagamer5903@gmail.com>
2025-12-14 14:42:08 -0300

    Create AnimeFirePlugin.kt

---
+++ AnimeFire/src/main/kotlin/com/AnimeFire/AnimeFirePlugin.kt
@@ -0,0 +1,12 @@
+package com.AnimeFire
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class AnimeFireProviderPlugin: Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(AnimeFire())
+    }
+}
