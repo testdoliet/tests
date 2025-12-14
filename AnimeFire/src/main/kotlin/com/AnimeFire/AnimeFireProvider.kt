9ec09e34eba2e228b83c71e41d1f82d5d7625bab
testdoliet <contagamer5903@gmail.com>
2025-12-14 14:58:03 -0300

    Update AnimeFireProvider.kt

--- AnimeFire/src/main/kotlin/com/AnimeFire/AnimeFireProvider.kt
+++ AnimeFire/src/main/kotlin/com/AnimeFire/AnimeFireProvider.kt
@@ -7,6 +7,7 @@ import com.lagradost.cloudstream3.plugins.Plugin
 @CloudstreamPlugin
 class AnimeFireProvider: Plugin() {
     override fun load(context: Context) {
+ 
         registerMainAPI(AnimeFire())
     }
 }
