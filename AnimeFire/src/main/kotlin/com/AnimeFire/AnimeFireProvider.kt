f5bb31c0ee210bf471835eb573fb7735e055d01c
testdoliet <contagamer5903@gmail.com>
2025-12-17 12:22:51 -0300

    Update AnimeFireProvider.kt

--- AnimeFire/src/main/kotlin/com/AnimeFire/AnimeFireProvider.kt
+++ AnimeFire/src/main/kotlin/com/AnimeFire/AnimeFireProvider.kt
@@ -7,7 +7,24 @@ import com.lagradost.cloudstream3.plugins.Plugin
 @CloudstreamPlugin
 class AnimeFireProvider: Plugin() {
     override fun load(context: Context) {
- 
+        // Registrar a API principal
         registerMainAPI(AnimeFire())
+        
+        // Registrar extractor como um ExtractorApi personalizado
+        registerExtractorAPI(object : ExtractorApi() {
+            override val name = "AnimeFire"
+            override val mainUrl = "https://animefire.io"
+            
+            override suspend fun getUrl(
+                url: String,
+                referer: String?,
+                subtitleCallback: (SubtitleFile) -> Unit,
+                callback: (ExtractorLink) -> Unit
+            ): Boolean {
+                return AnimeFireExtractor.extractVideoLinks(url, mainUrl, name) { link ->
+                    callback(link)
+                }
+            }
+        })
     }
 }
