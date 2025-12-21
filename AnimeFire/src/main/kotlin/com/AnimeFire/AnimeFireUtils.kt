1dd16684262c59683528ebc74f137b0838dd5d3a
testdoliet <contagamer5903@gmail.com>
2025-12-21 06:39:12 -0300

    Update AnimeFireUtils.kt

--- AnimeFire/src/main/kotlin/com/AnimeFire/AnimeFireUtils.kt
+++ AnimeFire/src/main/kotlin/com/AnimeFire/AnimeFireUtils.kt
@@ -65,4 +65,21 @@ object AnimeFireUtils {
             cleanTitle
         }
     }
+fun getStatus(t: String?): ShowStatus {
+    return when {
+        t == null -> ShowStatus.Completed
+        t.contains("em andamento", ignoreCase = true) || 
+        t.contains("lançando", ignoreCase = true) ||
+        t.contains("lançamento", ignoreCase = true) ||
+        t.contains("updating", ignoreCase = true) ||
+        t.contains("ongoing", ignoreCase = true) -> ShowStatus.Ongoing
+        
+        t.contains("concluído", ignoreCase = true) ||
+        t.contains("completo", ignoreCase = true) ||
+        t.contains("completado", ignoreCase = true) ||
+        t.contains("finished", ignoreCase = true) -> ShowStatus.Completed
+        
+        else -> ShowStatus.Completed
+    }
+  }
 }
