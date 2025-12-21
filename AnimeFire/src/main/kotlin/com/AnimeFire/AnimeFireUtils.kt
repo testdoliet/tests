42395d6fe05cd0cd74674f16a1dfb3c80fab03ce
testdoliet <contagamer5903@gmail.com>
2025-12-21 07:05:53 -0300

    Update AnimeFireUtils.kt

--- AnimeFire/src/main/kotlin/com/AnimeFire/AnimeFireUtils.kt
+++ AnimeFire/src/main/kotlin/com/AnimeFire/AnimeFireUtils.kt
@@ -5,16 +5,8 @@ import com.lagradost.cloudstream3.ShowStatus
 fun getStatus(t: String?): ShowStatus {
     return when {
         t == null -> ShowStatus.Completed
-        t.contains("em andamento", ignoreCase = true) || 
-        t.contains("lançando", ignoreCase = true) ||
-        t.contains("lançamento", ignoreCase = true) ||
-        t.contains("updating", ignoreCase = true) ||
-        t.contains("ongoing", ignoreCase = true) -> ShowStatus.Ongoing
-        
-        t.contains("concluído", ignoreCase = true) ||
-        t.contains("completo", ignoreCase = true) ||
-        t.contains("completado", ignoreCase = true) ||
-        t.contains("finished", ignoreCase = true) -> ShowStatus.Completed
+        t.contains("em lançamento", ignoreCase = true) -> ShowStatus.Ongoing
+        t.contains("concluído", ignoreCase = true) -> ShowStatus.Completed
         
         else -> ShowStatus.Completed
     }
