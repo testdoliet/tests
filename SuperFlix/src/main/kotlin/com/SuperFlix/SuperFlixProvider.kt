a385ec2148d7b14ad84fdc916d0b30767b36fe0f
testdoliet <contagamer5903@gmail.com>
2025-12-23 19:58:55 -0300

    Update SuperFlixProvider.kt

--- SuperFlix/src/main/kotlin/com/SuperFlix/SuperFlixProvider.kt
+++ SuperFlix/src/main/kotlin/com/SuperFlix/SuperFlixProvider.kt
@@ -7,13 +7,6 @@ import com.lagradost.cloudstream3.plugins.Plugin
 @CloudstreamPlugin
 class SuperFlixProviderPlugin: Plugin() {
     override fun load(context: Context) {
-        println("SuperFlixProviderPlugin: load - INICIANDO PLUGIN")
-
-        // Registrar o provider principal
         registerMainAPI(SuperFlix())
-        println("SuperFlixProviderPlugin: load - SuperFlix provider registrado")
-
-        // Não é necessário registrar extractors separadamente para providers customizados
-        println("SuperFlixProviderPlugin: load - Plugin carregado com sucesso")
     }
 }
