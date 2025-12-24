4329eca3b74a74acbcbb296cc62dabf6676ade48
testdoliet <contagamer5903@gmail.com>
2025-12-24 06:20:15 -0300

    Update AnimeFireProvider.kt

--- AnimeFire/src/main/kotlin/com/AnimeFire/AnimeFireProvider.kt
+++ AnimeFire/src/main/kotlin/com/AnimeFire/AnimeFireProvider.kt
@@ -7,13 +7,6 @@ import com.lagradost.cloudstream3.plugins.Plugin
 @CloudstreamPlugin
 class AnimeFireProvider: Plugin() {
     override fun load(context: Context) {
-        println("AnimeFireProvider: load - INICIANDO PLUGIN")
-
-        // Registrar o provider principal
         registerMainAPI(AnimeFire())
-        println("AnimeFireProvider: load - AnimeFire provider registrado")
-
-        // Não é necessário registrar extractors separadamente
-        println("AnimeFireProvider: load - Plugin carregado com sucesso")
     }
 }
