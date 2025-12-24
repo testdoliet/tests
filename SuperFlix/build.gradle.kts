ccaa31762cb0510a2c5dfc227bb716c3d01a045c
testdoliet <contagamer5903@gmail.com>
2025-12-24 10:19:20 -0300

    Update build.gradle.kts

--- SuperFlix/build.gradle.kts
+++ SuperFlix/build.gradle.kts
@@ -1,26 +1,31 @@
+import java.util.Properties
+
 plugins {
     kotlin("android")
     id("com.android.library")
 }
 
 android {
+   buildFeatures {
+        buildConfig = true
+    }
     namespace = "com.SuperFlix"
     compileSdk = 33
 
     defaultConfig {
         minSdk = 24
         targetSdk = 33
-        
+
         val tmdbApiKey = project.findProperty("TMDB_API_KEY") as? String
             ?: System.getenv("TMDB_API_KEY")
             ?: getLocalProperty("TMDB_API_KEY")
             ?: "dummy_api_key"
-        
+
         val tmdbAccessToken = project.findProperty("TMDB_ACCESS_TOKEN") as? String
             ?: System.getenv("TMDB_ACCESS_TOKEN")
             ?: getLocalProperty("TMDB_ACCESS_TOKEN")
             ?: "dummy_access_token"
-        
+
         buildConfigField("String", "TMDB_API_KEY", "\"$tmdbApiKey\"")
         buildConfigField("String", "TMDB_ACCESS_TOKEN", "\"$tmdbAccessToken\"")
     }
@@ -31,12 +36,12 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"))
         }
     }
-    
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
@@ -45,7 +50,7 @@ android {
 fun getLocalProperty(key: String): String? {
     val localProperties = Properties()
     val localPropertiesFile = project.rootProject.file("local.properties")
-    
+
     return if (localPropertiesFile.exists()) {
         localProperties.load(localPropertiesFile.inputStream())
         localProperties.getProperty(key)
