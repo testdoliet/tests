8f08ed52c8e75024d4221a1f3d9c097faf47584d
testdoliet <contagamer5903@gmail.com>
2025-12-26 12:37:14 -0300

    Update SuperFlixProvider.kt

--- SuperFlix/src/main/kotlin/com/SuperFlix/SuperFlixProvider.kt
+++ SuperFlix/src/main/kotlin/com/SuperFlix/SuperFlixProvider.kt
@@ -6,13 +6,8 @@ import android.content.Context
 
 @CloudstreamPlugin
 class SuperFlixProvider : Plugin() {
-    override fun load(context: Context) {
-        // Registra o provider principal
         registerMainAPI(SuperFlix())
-        
-        // REGISTRA O EXTRACTOR (agora é uma classe que extende ExtractorApi)
-        registerExtractorAPI(SuperFlixYoutubeExtractor())
-        
-        println("✅ SuperFlix Provider carregado com YouTube Extractor")
+        registerExtractorAPI(YouTubeTrailerExtractor())
     }
 }
+        
