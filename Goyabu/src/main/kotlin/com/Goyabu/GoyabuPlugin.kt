760ff198328591b8531fbb89b47e2b9480954f5b
testdoliet <contagamer5903@gmail.com>
2025-12-29 08:57:28 -0300

    Update GoyabuPlugin.kt

--- Goyabu/src/main/kotlin/com/Goyabu/GoyabuPlugin.kt
+++ Goyabu/src/main/kotlin/com/Goyabu/GoyabuPlugin.kt
@@ -7,12 +7,7 @@ import com.lagradost.cloudstream3.plugins.Plugin
 @CloudstreamPlugin
 class GoyabuProviderPlugin: Plugin() {
     override fun load(context: Context) {
-        println("🎬 GOYABU: Iniciando plugin...")
         
-        // Registrar o provider principal
-        registerMainAPI(Goyabu())
-        println("🎬 GOYABU: Provider registrado com sucesso")
-        
-        println("✅ GOYABU: Plugin carregado - 18 gêneros disponíveis")
+        registerMainAPI(Goyabu)())
     }
 }
