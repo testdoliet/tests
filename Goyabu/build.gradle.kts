c7228927be8ce99894a1ee43b241c5fa7440ad1c
testdoliet <contagamer5903@gmail.com>
2025-12-29 09:00:37 -0300

    Update build.gradle.kts

--- Goyabu/build.gradle.kts
+++ Goyabu/build.gradle.kts
@@ -1,57 +1,11 @@
-plugins {
-    kotlin("android")
-    id("com.android.library")
-}
-
-android {
-    buildFeatures {
-    buildConfig = true
-    namespace = "com.SuperFlix"
-    compileSdk = 33
-}
-    defaultConfig {
-        minSdk = 24
-        targetSdk = 33
-
-        // VARIÁVEIS DE BUILD PARA TMDB
-        // API KEY: Usada em queries (?api_key=xxx)
-        buildConfigField("String", "TMDB_API_KEY", 
-            "\"${System.getenv("TMDB_API_KEY") ?: "dummy_api_key"}\"")
-        
-        // ACCESS TOKEN: Usada em headers (Authorization: Bearer xxx)
-        buildConfigField("String", "TMDB_ACCESS_TOKEN", 
-            "\"${System.getenv("TMDB_ACCESS_TOKEN") ?: "dummy_access_token"}\"")
-    }
-
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"))
-        }
-    }
-    
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-}
+version = 1
 
 cloudstream {
-    description = "Site brasileiro de animes em FHD"
+    description = "Animes Online em HD gratuitamente no Goyabu"
     language = "pt-br"
     authors = listOf("lawlietbr")
     status = 1
     tvTypes = listOf("Animes")
     iconUrl = "https://goyabu.io/wp-content/uploads/2025/12/cropped-goyabu-favicon-192x192.png"
     isCrossPlatform = true
-    requiresResources = true
-}
-
-dependencies {
-    val cloudstream by configurations
-    cloudstream("com.lagradost:cloudstream3:pre-release")
 }
