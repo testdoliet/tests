365471c67946d5b355d96cc243455aa2d274ec5d
testdoliet <contagamer5903@gmail.com>
2025-12-29 11:53:39 -0300

    Create build.gradle.kts

---
+++ AniTube/build.gradle.kts
@@ -0,0 +1,11 @@
+version = 1
+
+cloudstream {
+    description = "AniTube é um site para assistir seus animes favoritos online e de onde quiser, temos uma lista completa com diversos animes recentes e antigos para você explorar e o melhor disso tudo é a qualidade do nosso site que o diferencia de outros."
+    language = "pt-br"
+    authors = listOf("lawlietbr")
+    status = 1
+    tvTypes = listOf("Animes")
+    iconUrl = "https://www.anitube.news/wp-content/uploads/cropped-Favicon6-192x192.png"
+    isCrossPlatform = true
+}
