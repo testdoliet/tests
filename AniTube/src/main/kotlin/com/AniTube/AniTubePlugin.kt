68b1a8887952744cce884e1ae052becf92e5b84f
testdoliet <contagamer5903@gmail.com>
2025-12-29 11:55:40 -0300

    Create AniTubePlugin.kt

---
+++ AniTube/src/main/kotlin/com/AniTube/AniTubePlugin.kt
@@ -0,0 +1,13 @@
+package com.AniTube
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class AniTubePlugin: Plugin() {
+    override fun load(context: Context) {
+
+        registerMainAPI(AniTube())
+    }
+}
