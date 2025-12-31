337416aea6d138937acab8d3dc1fd952d42591ba
testdoliet <contagamer5903@gmail.com>
2025-12-31 05:59:27 -0300

    Create build.gradle.kts

---
+++ Hypeflix/build.gradle.kts
@@ -0,0 +1,77 @@
+import java.util.Properties
+
+plugins {
+    kotlin("android")
+    id("com.android.library")
+}
+
+android {
+    buildFeatures {
+        buildConfig = true
+    }
+    namespace = "com.HypeFlix"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 24
+        targetSdk = 33
+
+        val tmdbApiKey = project.findProperty("TMDB_API_KEY") as? String
+            ?: System.getenv("TMDB_API_KEY")
+            ?: getLocalProperty("TMDB_API_KEY")
+            ?: "dummy_api_key"
+
+        val tmdbAccessToken = project.findProperty("TMDB_ACCESS_TOKEN") as? String
+            ?: System.getenv("TMDB_ACCESS_TOKEN")
+            ?: getLocalProperty("TMDB_ACCESS_TOKEN")
+            ?: "dummy_access_token"
+
+        buildConfigField("String", "TMDB_API_KEY", "\"$tmdbApiKey\"")
+        buildConfigField("String", "TMDB_ACCESS_TOKEN", "\"$tmdbAccessToken\"")
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"))
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+fun getLocalProperty(key: String): String? {
+    val localProperties = Properties()
+    val localPropertiesFile = project.rootProject.file("local.properties")
+
+    return if (localPropertiesFile.exists()) {
+        localProperties.load(localPropertiesFile.inputStream())
+        localProperties.getProperty(key)
+    } else {
+        null
+    }
+}
+
+cloudstream {
+    version = 1 
+    description = "Hypeflix é o seu destino nº1 para assistir filmes online grátis e séries online, em alta qualidade e sem complicações."
+    language = "pt-br"
+    authors = listOf("lawlietbr") 
+    status = 1 
+    tvTypes = listOf("Movies", "Series", "Animes")
+    iconUrl = "https://hypeflix.info/uploads/images/logo_hypeflix.png"
+    isCrossPlatform = true
+    requiresResources = true
+}
+
+dependencies {
+    val cloudstream by configurations
+    cloudstream("com.lagradost:cloudstream3:pre-release")
+}
