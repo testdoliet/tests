bd347ceaec66517edc2b590548607de29d70e8b9
testdoliet <contagamer5903@gmail.com>
2026-01-06 07:09:37 -0300

    Create SuperflixPlugin.kt

---
+++ Superflix/src/main/kotlin/com/Superflix/SuperflixPlugin.kt
@@ -0,0 +1,13 @@
+package com.Superflix
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class SuperflixProvider : Plugin() {
+    override fun load(context: Context) {
+
+        registerMainAPI(Superflix())
+    }
+}
