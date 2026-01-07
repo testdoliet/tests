da51f800ba45f71ee85acbddfa53412f1a45f77c
testdoliet <contagamer5903@gmail.com>
2026-01-07 12:43:25 -0300

    Update NexflixPlugin.kt

--- Nexflix/src/main/kotlin/com/Nexflix/NexflixPlugin.kt
+++ Nexflix/src/main/kotlin/com/Nexflix/NexflixPlugin.kt
@@ -7,7 +7,10 @@ import android.content.Context
 @CloudstreamPlugin
 class NexFlixPlugin: Plugin() {
     override fun load(context: Context) {
-        
+        // Registra o provedor principal (Site, Busca, Catálogo)
         registerMainAPI(NexFlix())
+
+        // Registra o extractor (Lógica para tirar o m3u8 do player)
+        registerExtractorAPI(NexEmbedExtractor())
     }
 }
