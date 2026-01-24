b6dba5e8bad6b8ab13c9c93ff77abf684938c6d6
testdoliet <contagamer5903@gmail.com>
2026-01-24 05:51:40 -0300

    Create build.gradle.kts

---
+++ TopAnimes/build.gradle.kts
@@ -0,0 +1,11 @@
+version = 1
+
+cloudstream {
+    description = "TOPANIMES.NET - Animes Online Grátis em FHD!"
+    language = "pt-br"
+    authors = listOf("lawlietbr")
+    status = 1
+    tvTypes = listOf("Animes")
+    iconUrl = "https://topanimes.net/wp-content/uploads/2025/11/1009988.png"
+    isCrossPlatform = true
+}
