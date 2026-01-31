d4ecd5c89902d2e096d86a0cba9835a1da098a8e
testdoliet <contagamer5903@gmail.com>
2026-01-31 09:34:09 -0300

    Create build.gradle.kts

---
+++ AnimeQ/src/main/kotlin/com/AnimeQ/build.gradle.kts
@@ -0,0 +1,11 @@
+version = 1
+
+cloudstream {
+    description = "AnimeQ é um site para assistir seus animes favoritos online e de onde quiser, temos uma lista completa com diversos animes recentes e antigos para você explorar, incluindo animes legendados, dublados, filmes, tokusatsus e mais. A melhor maneira de assistir animes online grátis em HD sempre atualizados!"
+    language = "pt-br"
+    authors = listOf("lawlietbr")
+    status = 1
+    tvTypes = listOf("Anime", "AnimeMovie")
+    iconUrl = "https://animeq.net/wp-content/uploads/2025/06/Favicon-AnimeQ-1-150x150.png"
+    isCrossPlatform = true
+}
