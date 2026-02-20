72f02ec9cb26f95c034ca4b435fe33933ed40f5f
testdoliet <contagamer5903@gmail.com>
2026-02-20 09:10:43 -0300

    Create build.gradle.kts

---
+++ DattebayoBR/build.gradle.kts
@@ -0,0 +1,11 @@
+version = 1
+
+cloudstream {
+    description = "DattebayoBR Animes Online"
+    language = "pt-br"
+    authors = listOf("lawlietbr")
+    status = 1
+    tvTypes = listOf("Anime, AsianDrama")
+    iconUrl = "https://www.dattebayo-br.com/favicon.png"
+    isCrossPlatform = true
+}
