62018f5d968e2d96759e6a65382772c2d046179a
testdoliet <contagamer5903@gmail.com>
2026-02-22 16:50:30 -0300

    Update build.gradle.kts

--- HinataSoul/build.gradle.kts
+++ HinataSoul/build.gradle.kts
@@ -6,6 +6,6 @@ cloudstream {
     authors = listOf("lawlietbr")
     status = 1
     tvTypes = listOf("Anime, AsianDrama")
-    iconUrl = "https://www.hinatasoul.com/favicon.png"
+    iconUrl = "https://www.hinatasoul.com/favicon-32x32.png"
     isCrossPlatform = true
 }
