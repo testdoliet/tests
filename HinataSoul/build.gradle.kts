a97274e1621d5bc57842aa51ff9d6077f54641de
testdoliet <contagamer5903@gmail.com>
2026-02-22 16:49:05 -0300

    Create build.gradle.kts

---
+++ HinataSoul/build.gradle.kts
@@ -0,0 +1,11 @@
+version = 1
+
+cloudstream {
+    description = "hinatasoul.com"
+    language = "pt-br"
+    authors = listOf("lawlietbr")
+    status = 1
+    tvTypes = listOf("Anime, AsianDrama")
+    iconUrl = "https://www.hinatasoul.com/favicon.png"
+    isCrossPlatform = true
+}
