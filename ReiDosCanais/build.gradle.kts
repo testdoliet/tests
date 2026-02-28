f91be024c4e7d5659a99c6660d9b1d5636f54716
testdoliet <contagamer5903@gmail.com>
2026-02-28 07:08:56 -0300

    Create build.gradle.kts

---
+++ ReiDosCanais/build.gradle.kts
@@ -0,0 +1,11 @@
+{
+    "name": "Rei dos Canais",
+    "description": "Assista TV ao vivo gratuitamente no Rei dos Canais. Canais de esportes, entretenimento, notícias, filmes e séries gratuitos. Eventos esportivos ao vivo sem cadastro, direto do seu navegador.",
+    "version": 1,
+    "language": "pt-br",
+    "authors": "lawliet",
+    "status": 1,
+    "tvTypes": "Live",
+    "iconUrl": "https://reidoscanais.io/favicon.png",
+    "isCrossPlatform": true
+}
