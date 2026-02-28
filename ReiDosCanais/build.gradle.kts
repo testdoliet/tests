c76019aeddad3f8620f986732d7bfd10d3726cf2
testdoliet <contagamer5903@gmail.com>
2026-02-28 07:15:32 -0300

    Update build.gradle.kts

--- ReiDosCanais/build.gradle.kts
+++ ReiDosCanais/build.gradle.kts
@@ -1,11 +1,11 @@
-{
-    "name": "Rei dos Canais",
-    "description": "Assista TV ao vivo gratuitamente no Rei dos Canais. Canais de esportes, entretenimento, notícias, filmes e séries gratuitos. Eventos esportivos ao vivo sem cadastro, direto do seu navegador.",
-    "version": 1,
-    "language": "pt-br",
-    "authors": "lawliet",
-    "status": 1,
-    "tvTypes": "Live",
-    "iconUrl": "https://reidoscanais.io/favicon.png",
-    "isCrossPlatform": true
+version = 1
+
+cloudstream {
+    description = "Assista TV ao vivo gratuitamente no Rei dos Canais. Canais de esportes, entretenimento, notícias, filmes e séries gratuitos. Eventos esportivos ao vivo sem cadastro, direto do seu navegador."
+    language = "pt-br"
+    authors = listOf("lawlietbr")
+    status = 1
+    tvTypes = listOf("Live")
+    iconUrl = "https://reidoscanais.io/favicon.png"
+    isCrossPlatform = true
 }
