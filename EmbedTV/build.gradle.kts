b38e1c23f6be6b42b2ff010a54b630a4f9214285
testdoliet <contagamer5903@gmail.com>
2026-03-14 07:17:07 -0300

    Create build.gradle.kts

---
+++ EmbedTV/build.gradle.kts
@@ -0,0 +1,11 @@
+version = 1
+
+cloudstream {
+    description = "Embed TV - O melhor site de TV online. "
+    language = "pt-br"
+    authors = listOf("lawlietbr")
+    status = 1
+    tvTypes = listOf("Live")
+    iconUrl = "https://embedtv.best/assets/icon.png"
+    isCrossPlatform = true
+}
