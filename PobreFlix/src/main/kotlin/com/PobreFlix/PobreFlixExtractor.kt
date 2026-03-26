575235bc133a09f03946643356adb059fc448556
testdoliet <contagamer5903@gmail.com>
2026-03-26 06:06:33 -0300

    Update PobreFlixExtractor.kt

--- PobreFlix/src/main/kotlin/com/PobreFlix/PobreFlixExtractor.kt
+++ PobreFlix/src/main/kotlin/com/PobreFlix/PobreFlixExtractor.kt
@@ -5,7 +5,7 @@ import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.ExtractorLinkType
 import com.lagradost.cloudstream3.utils.M3u8Helper
 import com.lagradost.cloudstream3.utils.newExtractorLink
-import com.lagradost.cloudstream3.utils.NiceResponse  // ← Import correto
+import com.lagradost.cloudstream3.utils.Response  // Importação correta
 import org.json.JSONObject
 import java.net.URLEncoder
 
@@ -36,9 +36,8 @@ object PobreFlixExtractor {
         "Connection" to "keep-alive"
     )
     
-    // CORRIGIDO: parâmetro agora é NiceResponse e acesso via headers como Map
-    private fun updateCookies(response: NiceResponse) {
-        val setCookie = response.headers["set-cookie"]  // ← Map access
+    private fun updateCookies(response: Response) {
+        val setCookie = response.headers["set-cookie"]
         if (setCookie != null) {
             sessionCookies = setCookie
         }
