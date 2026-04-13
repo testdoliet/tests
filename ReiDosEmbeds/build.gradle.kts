51bf2af6054cc33c384e1f21ff1a7b80e1fa4d60
testdoliet <contagamer5903@gmail.com>
2026-04-13 19:56:14 -0300

    Create build.gradle.kts

---
+++ ReiDosEmbeds/build.gradle.kts
@@ -0,0 +1,11 @@
+version = 1
+
+cloudstream {
+    description = "Rei dos Embeds - O melhor diretório de canais ao vivo. Mais de 150 canais disponíveis."
+    language = "pt-br"
+    authors = listOf("lawlietbr")
+    status = 1
+    tvTypes = listOf("Live")
+    iconUrl = "https://reidosembeds.com/favicon.ico"
+    isCrossPlatform = true
+}
