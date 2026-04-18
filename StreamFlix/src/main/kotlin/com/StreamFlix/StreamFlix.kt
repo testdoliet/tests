11e7072a8397c7df0db6a48eb1393ad0af5935d2
testdoliet <contagamer5903@gmail.com>
2026-04-18 05:37:59 -0300

    Update StreamFlix.kt

--- StreamFlix/src/main/kotlin/com/StreamFlix/StreamFlix.kt
+++ StreamFlix/src/main/kotlin/com/StreamFlix/StreamFlix.kt
@@ -49,18 +49,18 @@ class StreamFlix : MainAPI() {
 
     private val SERIES_CATEGORIES = mapOf(
         "107" to "Séries - Netflix", "109" to "Séries - Apple TV",
-        "112" to "Séries - Crunchyroll",
+        "112" to "Séries - Crunchyroll", "114" to "Séries - Discovery",
         "115" to "Séries - Disney Plus", "126" to "Séries - Doramas",
         "117" to "Séries - Globoplay", "119" to "Séries - Legendadas",
-        "118" to "Séries - Max (HBO Max)", 
+        "118" to "Séries - Max (HBO Max)", "123" to "Séries - Paramount+"
     )
 
     private val genreMap = mapOf(
         28 to "Ação", 12 to "Aventura", 16 to "Animação", 35 to "Comédia",
-        80 to "Crime", 99 to "Documentário", 18 to "Drama", 
-          27 to "Terror", 
-        9648 to "Mistério", 10749 to "Romance", 878 to "Ficção Científica"
-    
+        80 to "Crime", 99 to "Documentário", 18 to "Drama", 10751 to "Família",
+        14 to "Fantasia", 36 to "História", 27 to "Terror", 10402 to "Música",
+        9648 to "Mistério", 10749 to "Romance", 878 to "Ficção Científica",
+        53 to "Thriller", 10752 to "Guerra", 37 to "Faroeste"
     )
 
     override val mainPage = mainPageOf(
