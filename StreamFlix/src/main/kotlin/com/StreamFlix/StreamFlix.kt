3ef78e12c61fedef294613ea03453a7cd836baba
testdoliet <contagamer5903@gmail.com>
2026-04-18 07:19:14 -0300

    Update StreamFlix.kt

--- StreamFlix/src/main/kotlin/com/StreamFlix/StreamFlix.kt
+++ StreamFlix/src/main/kotlin/com/StreamFlix/StreamFlix.kt
@@ -801,3 +801,18 @@ class StreamFlix : MainAPI() {
 
     private data class TMDBCast(
         @JsonProperty("name") val name: String,
+        @JsonProperty("character") val character: String?,
+        @JsonProperty("profile_path") val profile_path: String?
+    )
+
+    private data class TMDBVideos(
+        @JsonProperty("results") val results: List<TMDBVideo>
+    )
+
+    private data class TMDBVideo(
+        @JsonProperty("key") val key: String,
+        @JsonProperty("site") val site: String,
+        @JsonProperty("type") val type: String,
+        @JsonProperty("official") val official: Boolean? = false
+    )
+}
