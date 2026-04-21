65e813074c58d8ab84a79acb33273e39e8e2f9b6
testdoliet <contagamer5903@gmail.com>
2026-04-21 09:12:26 -0300

    Update build.gradle.kts

--- MendigoFlix/build.gradle.kts
+++ MendigoFlix/build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    kotlin("android")
     id("com.android.library")
 }
 
@@ -8,14 +7,8 @@ android {
     compileSdk = 33
 
     defaultConfig {
-        minSdk = 24
-        targetSdk = 33
-
-        val tmdbApiKey = System.getenv("TMDB_API_KEY") ?: "dummy_api_key"
-        val tmdbAccessToken = System.getenv("TMDB_ACCESS_TOKEN") ?: "dummy_access_token"
-
+        val tmdbApiKey = System.getenv("TMDB_API_KEY") ?: "dummy"
         buildConfigField("String", "TMDB_API_KEY", "\"$tmdbApiKey\"")
-        buildConfigField("String", "TMDB_ACCESS_TOKEN", "\"$tmdbAccessToken\"")
     }
 }
 
@@ -27,11 +20,4 @@ cloudstream {
     status = 1
     tvTypes = listOf("Movies", "Series", "Animes")
     iconUrl = "https://mendigoflix.lol/assets/favicon.png"
-}
-
-dependencies {
-    val cloudstream by configurations
-    cloudstream("com.lagradost:cloudstream3:pre-release")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
 }
