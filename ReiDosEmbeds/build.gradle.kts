7e6fd40e39b1be130cd455cf49723e4c270b14bf
testdoliet <contagamer5903@gmail.com>
2026-04-21 10:11:30 -0300

    Update build.gradle.kts

--- ReiDosEmbeds/build.gradle.kts
+++ ReiDosEmbeds/build.gradle.kts
@@ -7,5 +7,4 @@ cloudstream {
     status = 1
     tvTypes = listOf("Live")
     iconUrl = "https://www.image2url.com/r2/default/images/1776423716151-20e5aa74-a764-4d60-8575-0851d8ab37df.png"
-    isCrossPlatform = true
 }
