a5e073b26324236dfb5ebb3a40fcb0b7f6a23dc0
testdoliet <contagamer5903@gmail.com>
2026-04-21 10:08:42 -0300

    Update build.gradle.kts

--- Goyabu/build.gradle.kts
+++ Goyabu/build.gradle.kts
@@ -7,5 +7,4 @@ cloudstream {
     status = 1
     tvTypes = listOf("Animes")
     iconUrl = "https://goyabu.io/wp-content/uploads/2025/12/cropped-goyabu-favicon-192x192.png"
-    isCrossPlatform = true
 }
